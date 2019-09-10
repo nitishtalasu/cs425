c5f06e531723ce82fa4c7b8aff781c949249acce
Prateeth Reddy <prateeth@Prateeths-MacBook-Pro.local>
2019-09-09 22:19:16 -0500

    client multithreading

--- Client.java
+++ Client.java
@@ -1,39 +1,69 @@
-package cs425;
 
 import java.net.*; 
 import java.io.*; 
-  
+@SuppressWarnings("deprecation")
 public class Client 
 { 
     // initialize socket and input output streams 
-    private Socket socket            = null; 
+    // private Socket socket            = null; 
     private DataInputStream  input   = null; 
     private DataOutputStream out     = null; 
-  
+    private String address = null;
+    private int port;
+   // private BufferedReader reader=null;
     // constructor to put ip address and port 
     public Client(String address, int port) 
-    { 
+    {   
+        this.address =  address;
+        this.port = port;
+       
+    }
+    public void create_thread() {
+    //   try{
         // establish a connection 
+        while(true) {
         try
         { 
-            socket = new Socket(address, port); 
+            Socket socket = new Socket(address, port); 
             System.out.println("Connected"); 
+             // takes input from terminal 
+             input  = new DataInputStream(System.in); 
   
-            // takes input from terminal 
-            input  = new DataInputStream(System.in); 
-  
-            // sends output to the socket 
-            out    = new DataOutputStream(socket.getOutputStream()); 
-        } 
-        catch(UnknownHostException u) 
+             // sends output to the socket 
+             out    = new DataOutputStream(socket.getOutputStream()); 
+             Thread t = new ClientThread(socket, input, out);
+
+             t.start(); 
+        }
+        catch(Exception e) 
         { 
-            System.out.println(u); 
+            System.out.println(e); 
         } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
+    }
+}
+}
+// ClientThread class 
+class ClientThread extends Thread  
+{ 
+    private DataInputStream input = null; 
+    private DataOutputStream out = null; 
+    private Socket socket = null; 
+      
   
+    // Constructor 
+    public ClientThread(Socket socket, DataInputStream input, DataOutputStream out)  
+    { 
+        this.socket = socket; 
+        this.input = input; 
+        this.out = out; 
+    } 
+  
+    @Override
+    public void run()  
+    { 
+        System.out.println("Client thread started: " + socket); 
+
+
         // string to read message from input 
         String line = ""; 
   
@@ -42,8 +72,8 @@ public class Client
         { 
             try
             { 
-                line = input.readLine(); 
-                out.writeUTF(line); 
+                line = this.input.readLine(); 
+                this.out.writeUTF(line); 
             } 
             catch(IOException i) 
             { 
@@ -54,18 +84,14 @@ public class Client
         // close the connection 
         try
         { 
-            input.close(); 
-            out.close(); 
-            socket.close(); 
+            this.input.close(); 
+            this.out.close(); 
+            this.socket.close(); 
         } 
         catch(IOException i) 
         { 
             System.out.println(i); 
         } 
-    } 
-  
-    public static void main(String args[]) 
-    { 
-        Client client = new Client("192.168.0.12", 5000); 
-    } 
-} 
+    }
+}
+
---
+++ ClientMain.java
@@ -0,0 +1,12 @@
+public class ClientMain {
+public static void main(String args[]) 
+{ 
+    String addresses[] = {"192.168.0.12"};
+    // Client client = new Client("192.168.0.12", 5000); 
+    // String addresses[] = {"0.0.0.0", "1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4"};
+    for(int i=0; i<addresses.length; i++) {
+        Client client = new Client(addresses[i], 5000); 
+        client.create_thread();
+    } 
+}
+} 
