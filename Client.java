58f5e7ff2e6645e5a0003c506a5b689b931adb2e
Prateeth Reddy <prateeth@Prateeths-MacBook-Pro.local>
2019-09-12 18:34:21 -0500

    update client files; add sample log

--- Client.java
+++ Client.java
@@ -1,16 +1,18 @@
 
-import java.net.*; 
 import java.io.*; 
+import java.text.*; 
+import java.util.*; 
+import java.net.*; 
 @SuppressWarnings("deprecation")
 public class Client 
 { 
-    // initialize socket and input output streams 
-    // private Socket socket            = null; 
-    private String  input   = null; 
+    // initialize input output streams 
+    private String input   = null; 
     private DataOutputStream out     = null; 
+    private DataInputStream in    = null; 
     private String address = null;
     private int port;
-   // private BufferedReader reader=null;
+
     // constructor to put ip address and port 
     public Client(String address, int port) 
     {   
@@ -19,29 +21,28 @@ public class Client
        
     }
     public void create_thread() {
-    //   try{
-        // establish a connection 
-   //     while(true) {
         try
         { 
             Socket socket = new Socket(address, port); 
             System.out.println("Connected"); 
-             // takes input from terminal 
-             //
-             input  = "Hello there"; 
-  
-             // sends output to the socket 
-             out    = new DataOutputStream(socket.getOutputStream()); 
-             Thread t = new ClientThread(socket, input, out);
+             
+            // takes input from terminal 
+            System.out.println("enter grep");
+            input = "-e [*y*]";
+              
+            // sends output to the socket 
+            in = new DataInputStream(socket.getInputStream());
+            
+            out    = new DataOutputStream(socket.getOutputStream()); 
+            Thread t = new ClientThread(socket, input, in, out);
 
-             t.start(); 
+            t.start(); 
         }
         catch(Exception e) 
         { 
             System.out.println(e); 
-        } 
-    //}
-}
+        }   
+    }
 }
 // ClientThread class 
 @SuppressWarnings("deprecation")
@@ -50,14 +51,15 @@ class ClientThread extends Thread
     private String input = ""; 
     private DataOutputStream out = null; 
     private Socket socket = null; 
+    private DataInputStream in = null; 
       
-  
     // Constructor 
-    public ClientThread(Socket socket, String input, DataOutputStream out)  
+    public ClientThread(Socket socket, String input, DataInputStream in, DataOutputStream out)  
     { 
         this.socket = socket; 
         this.input = input; 
         this.out = out; 
+        this.in = in;
     } 
   
     @Override
@@ -65,28 +67,32 @@ class ClientThread extends Thread
     { 
         System.out.println("Client thread started: " + socket); 
 
-
         // string to read message from input 
-       String line = ""; 
-  
-        // keep reading until "Over" is input 
-         
-         
+        String line = ""; 
+        String line2 = "";
+
             try
             { 
                 line = this.input; 
                 this.out.writeUTF(line); 
+
+                boolean eof = false;
+                while (!eof) {
+                    try {
+                        line2 = this.in.readUTF();
+                        System.out.println(line2);
+                    } catch (EOFException e) {
+                        eof = true;
+                    }
+                }   
             } 
             catch(IOException i) 
             { 
                 System.out.println(i); 
             } 
-        
-  
-        // close the connection 
         try
         { 
-         //   this.input.close(); 
+            this.in.close(); 
             this.out.close(); 
             this.socket.close(); 
         } 
--- ClientMain.java
+++ ClientMain.java
@@ -1,13 +1,11 @@
 @SuppressWarnings("deprecation")
 public class ClientMain {
-public static void main(String args[]) 
-{ 
-    String addresses[] = {"172.22.152.200", "172.22.156.195"};
-    // Client client = new Client("192.168.0.12", 5000); 
-    // String addresses[] = {"0.0.0.0", "1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4"};
-    for(int i=0; i<addresses.length; i++) {
-        Client client = new Client(addresses[i], 5000); 
-        client.create_thread();
-    } 
-}
+    public static void main(String args[]) 
+    { 
+        String addresses[] = {"192.168.0.12"};
+        for(int i=0; i<addresses.length; i++) {
+            Client client = new Client(addresses[i], 5000); 
+            client.create_thread();
+        } 
+    }
 } 
