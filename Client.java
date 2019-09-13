8a973904a9028131f8c3c4ca0cef60a15c43573f
Prateeth Reddy <prateeth@Prateeths-MacBook-Pro.local>
2019-09-13 18:47:37 -0500

    update client files

--- Client.java
+++ Client.java
@@ -5,44 +5,46 @@
 
 import java.io.*; 
 import java.net.Socket; 
-
+import java.util.Scanner;
 @SuppressWarnings("deprecation")
 public class Client 
 { 
     // initialize input stream,output stream, address and port 
-    private String input   = null; 
-    private DataOutputStream out     = null; 
-    private DataInputStream in    = null; 
+    private String clientInput = null; 
+    private DataOutputStream outputStream = null; 
+    private DataInputStream inputStream = null; 
     private String address = null;
+    private String vmId = null;
     private int port;
-    private FileWriter clientLog = null;
 
     // constructor to initialize ip address and port 
-    public Client(String address, int port) 
+    public Client(String address, String vmId, int port) 
     {   
-        this.address =  address;
+        this.address = address;
+        this.vmId = vmId;
         this.port = port;
        
     }
     public void create_thread() {
         try
         { 
+            Scanner sc = new Scanner(System.in);
             Socket socket = new Socket(address, port); 
             System.out.println("Connected"); 
-             
-            // takes input from terminal 
-            System.out.println("enter grep");
-            input = "-e ^[0-9]*[a-z]{5}";
               
             // sends output to the socket 
-            in = new DataInputStream(socket.getInputStream());
+            inputStream = new DataInputStream(socket.getInputStream());
+            outputStream = new DataOutputStream(socket.getOutputStream()); 
+
+            //clientInput = "-c -E ^[0-9]*[a-z]{5}";
+
+            System.out.println("Type grep command and press enter");
+            System.out.println("For example: -c -E ^[0-9]*[a-z]{5}");
+            clientInput = sc.nextLine();
+
+            sc.close();
             
-            out    = new DataOutputStream(socket.getOutputStream()); 
-
-            clientLog = new FileWriter("clientLog.log");
-
-            Thread t = new ClientThread(socket, input, in, out, logOutput, clientLog);
-
+            Thread t = new ClientThread(socket, clientInput, inputStream, outputStream, vmId);
             t.start(); 
         }
         catch(Exception e) 
@@ -53,10 +55,11 @@ public class Client
 
     public static void main(String args[]) 
     { 
-        String addresses[] = {"172.22.156.195","172.22.152.200","172.22.154.196","172.22.156.196","172.22.152.201","172.22.154.197","172.22.156.197","172.22.152.202","172.22.154.198"};
+        String addresses[] = {"172.22.154.195","172.22.156.195","172.22.152.200","172.22.154.196","172.22.156.196","172.22.152.201","172.22.154.197","172.22.156.197","172.22.152.202","172.22.154.198"};
+        String vmIds[] = {"vm1.log", "vm2.log", "vm3.log", "vm4.log", "vm5.log", "vm6.log", "vm7.log", "vm8.log", "vm9.log", "vm10.log"};
         for(int i=0; i<addresses.length; i++) {
-            Client client = new Client(addresses[i], 5000); 
+            Client client = new Client(addresses[i], vmIds[i], 5000); 
             client.create_thread();
-        } 
+        }
     }
 }
--- ClientThread.java
+++ ClientThread.java
@@ -1,43 +1,46 @@
+import java.io.*;
+import java.net.Socket;
+
 // ClientThread class 
 @SuppressWarnings("deprecation")
 class ClientThread extends Thread  
 { 
-    private String input = ""; 
-    private DataOutputStream out = null; 
+    private String clientInput = ""; 
+    private DataOutputStream outputStream = null; 
     private Socket socket = null; 
-    private DataInputStream in = null; 
-    private FileWriter clientLog = null;
+    private DataInputStream inputStream = null; 
+    private String vmId = "";
       
     // Constructor 
-    public ClientThread(Socket socket, String input, DataInputStream in, DataOutputStream out, FileWriter clientLog)  
+    public ClientThread(Socket socket, String clientInput, DataInputStream inputStream, DataOutputStream outputStream, String vmId)  
     { 
         this.socket = socket; 
-        this.input = input; 
-        this.out = out; 
-        this.in = in;
-        this.clientLog = clientLog;
+        this.clientInput = clientInput; 
+        this.outputStream = outputStream; 
+        this.inputStream = inputStream;
+        this.vmId = vmId;
     } 
   
     @Override
     public void run()  
     { 
         System.out.println("Client thread started: " + socket); 
+        long startTime = System.currentTimeMillis();
 
         // string to read message from input 
-        String line = ""; 
-        String line2 = "";
+        String lineOutputs = "";
 
             try
             { 
-                line = this.input; 
-                this.out.writeUTF(line); 
+                this.outputStream.writeUTF(this.clientInput);
+                this.outputStream.writeUTF(this.vmId);
 
                 boolean eof = false;
                 while (!eof) {
                     try {
-                        line2 = this.in.readUTF();
-                        // System.out.println(line2);
-                        clientLog.write(line2);
+                        lineOutputs = this.inputStream.readUTF();
+                        System.out.println(lineOutputs);
+                        //clientLog.write(line2);
                     } catch (EOFException e) {
                         eof = true;
                     }
@@ -49,9 +52,10 @@ class ClientThread extends Thread
             } 
         try
         { 
-            this.in.close(); 
-            this.out.close(); 
-            this.clientLog.close();
+            this.inputStream.close(); 
+            this.outputStream.close(); 
+            long endTime = System.currentTimeMillis();
+            System.out.println("thread runtime: " + (endTime - startTime));
             this.socket.close(); 
         } 
         catch(IOException i) 
