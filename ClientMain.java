0ead9f3b237d2bad4880b4ae4a9ff431bc6a3dd5
Chagari <chagari2@fa19-cs425-g59-01.cs.illinois.edu>
2019-09-13 16:29:24 -0500

    adding IP addresses of all servers

--- ClientMain.java
+++ ClientMain.java
@@ -2,7 +2,7 @@
 public class ClientMain {
     public static void main(String args[]) 
     { 
-        String addresses[] = {"192.168.0.12"};
+        String addresses[] = {"172.22.156.195","172.22.152.200","172.22.154.196","172.22.156.196","172.22.152.201","172.22.154.197","172.22.156.197","172.22.152.202","172.22.154.198"};
         for(int i=0; i<addresses.length; i++) {
             Client client = new Client(addresses[i], 5000); 
             client.create_thread();
